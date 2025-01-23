d5f3186b626ca14f7864e253925715682dc2930a
Santiago Garcia <just.b000ringsh1t@gmail.com>
2025-01-22 22:47:06 -0300

    feat: :sparkles: add basic auth with github

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
 import { type Config } from "drizzle-kit";
-
 import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
   dialect: "sqlite",
+  driver: "turso",
   dbCredentials: {
     url: env.DATABASE_URL,
+    authToken: env.DATABASE_TOKEN,
   },
   tablesFilter: ["advanced-todo_*"],
 } satisfies Config;
--- src/env.js
+++ src/env.js
@@ -10,6 +10,7 @@ export const env = createEnv({
     AUTH_GITHUB_ID: z.string(),
     AUTH_GITHUB_SECRET: z.string(),
     DATABASE_URL: z.string().url(),
+    DATABASE_TOKEN: z.string(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -22,6 +23,7 @@ export const env = createEnv({
     AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
     AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_TOKEN: process.env.DATABASE_TOKEN,
     NODE_ENV: process.env.NODE_ENV,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
