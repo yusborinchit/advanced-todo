04f2cdb1cd02395c073a9d989e9c81c6cf9ec01a
Santiago Garcia <just.b000ringsh1t@gmail.com>
2025-01-26 14:34:42 -0300

    feat: :sparkles: add active todos count

--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -11,9 +11,15 @@ export default async function TodoList({ userId }: Readonly<Props>) {
   const { success, data: allTodos } = await getTodos(userId);
 
   const firstLevelTodos = allTodos.filter((t) => t.parentId === -1);
+  const activeTodos = allTodos.filter((t) => !t.completed);
 
   return (
     <>
+      <div className="flex">
+        <span className="ml-auto text-sm text-neutral-500">
+          Active Todos: {activeTodos.length}
+        </span>
+      </div>
       <TodoForm parentId={-1} />
       <ul className="flex flex-col">
         {success &&
